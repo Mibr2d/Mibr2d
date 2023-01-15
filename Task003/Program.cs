dc264a624c8ec89130189e7ecdb789d908bec08d
mibr2d <hodakov7419@gmail.com>
2023-01-15 19:40:50 +0300

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿//3. Напишите программу вычисления функции:
+//x = f(a) Куб числа.
+
+Console.Write("Введите число > ");
+String stringValue = Console.ReadLine();
+int value = Convert.ToInt32(stringValue);
+
+int result = value * value * value;
+
+Console.WriteLine("Куб числа " + value + " равен " + result);
+Console.WriteLine($"Куб числа {value} равен {result}");
+
