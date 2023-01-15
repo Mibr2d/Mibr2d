5595d44cc6e2f2d015e5814eef2afee6b4e22455
mibr2d <hodakov7419@gmail.com>
2023-01-15 20:42:50 +0300

    Commit

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -6,11 +6,11 @@
 //-3->9
 //-7->49
 
-Console.Write("Введите число > ");
-String stringValue = Console.ReadLine();
-int value = Convert.ToInt32(stringValue);
+Console.Write("Введите число > ");//Вывод приглашения
+String stringValue = Console.ReadLine();//Прочитать строку
+int value = Convert.ToInt32(stringValue);//Преобразовываем строку в число
 
 int result = value * value;
 
 Console.WriteLine("Квадрат числа " + value + " равен " + result);
-System.Console.WriteLine($"Квадрат числа {value} равен {result}");
+Console.WriteLine($"Квадрат числа {value} равен {result}");
