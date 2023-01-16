437e9fbf432d615343d78edde193dcd773a975cf
mibr2d <hodakov7419@gmail.com>
2023-01-16 20:01:43 +0300

    Task009

---
+++ Task009/Program.cs
@@ -0,0 +1,31 @@
+﻿// 4. Напишите программу, которая принимает на вход 
+// число и проверяет, кратно ли оно одновременно 7 и 
+// 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int firstNumber = InputInt("Введите число");
+if (firstNumber%7==0)
+{
+    Console.WriteLine("Число кратно 7");
+}
+else
+{
+Console.WriteLine("Не кратно 7");
+}
+if(firstNumber%23==0)
+{
+Console.WriteLine("Число кратно 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно 23");
+}
