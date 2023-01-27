295701f5db09c2cac470919dace97ffd20a32cdd
mibr2d <hodakov7419@gmail.com>
2023-01-27 13:55:34 +0300

    Task003S4

---
+++ Task003S4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая 
+// принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int Factorial(int n)
+{
+    int Prod = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        Prod = Prod * i;
+    }
+    return Prod;
+
+}
+int number = Prompt("Введите число от 1 до ... ");
+
+int result = Factorial(number);
+System.Console.WriteLine($"Количество цифр в числе {number} равно {result}");
