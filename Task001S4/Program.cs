5d6bcd467d37cbb20369c42acaaa20a42dc9815c
mibr2d <hodakov7419@gmail.com>
2023-01-27 13:54:46 +0300

    Task001S4

---
+++ Task001S4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+double PromptDuoble(string message)
+{
+    System.Console.Write($"{message} >");
+    double result = Convert.ToDouble(Console.ReadLine());
+    return result;
+}
+
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int SumNumbersGauss(int number)
+{
+    return (number+1)*number/2;
+}
+
+int number = Prompt("Введите число");
+int sum = SumNumbers(number);
+System.Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
+System.Console.WriteLine($"Сумма чисел по Гауссу от 1 до {number} равна {SumNumbersGauss(number)}");
